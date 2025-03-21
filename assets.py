42ca33d484d9f1582d289c7829ff93bdb542d653
FelixSelter <felixselterdeveloping@gmail.com>
2025-03-21 18:04:28 +0100

    organized assets

--- assets.py
+++ assets.py
@@ -10,25 +10,27 @@ class Assets:
     def __load_all_assets(self):
         self.playerImgs = [self.__load_img(f"rsc/img/example{i}.bmp") for i in range(3)]
 
-        self.enemyImg_cow = self.__load_img("rsc/img/entities/cow.bmp")
-        self.enemyImgs_cow_left = [self.__load_img(f"rsc/img/entities/cow_animated{i+1}.bmp") for i in range(2)]
+        self.enemyImg_cow = self.__load_img("rsc/img/entities/cow/cow.bmp")
+        self.enemyImgs_cow_left = [self.__load_img(f"rsc/img/entities/cow/cow_animated{i + 1}.bmp") for i in range(2)]
         self.enemyImgs_cow_right = [pygame.transform.flip(image, True, False) for image in self.enemyImgs_cow_left]
 
-        self.enemyImg_sheep = self.__load_img("rsc/img/entities/sheep.bmp")
-        self.enemyImgs_sheep_left = [self.__load_img(f"rsc/img/entities/sheep_animated{i+1}.bmp") for i in range(3)]
+        self.enemyImg_sheep = self.__load_img("rsc/img/entities/sheep/sheep.bmp")
+        self.enemyImgs_sheep_left = [self.__load_img(f"rsc/img/entities/sheep/sheep_animated{i + 1}.bmp") for i in
+                                     range(3)]
         self.enemyImgs_sheep_right = [pygame.transform.flip(image, True, False) for image in self.enemyImgs_sheep_left]
 
-        self.enemyImg_pig = self.__load_img("rsc/img/entities/pig.bmp")
-        self.enemyImgs_pig_left = [self.__load_img(f"rsc/img/entities/pig_animated{i+1}.bmp") for i in range(2)]
+        self.enemyImg_pig = self.__load_img("rsc/img/entities/pig/pig.bmp")
+        self.enemyImgs_pig_left = [self.__load_img(f"rsc/img/entities/pig/pig_animated{i + 1}.bmp") for i in range(2)]
         self.enemyImgs_pig_right = [pygame.transform.flip(image, True, False) for image in self.enemyImgs_pig_left]
 
-        self.roosterImg = self.__load_img("rsc/img/entities/rooster.bmp")
-        self.roosterImgs_jump = [self.__load_img(f"rsc/img/entities/rooster_jump{i+1}.bmp") for i in range(2)]
+        self.roosterImg = self.__load_img("rsc/img/entities/rooster/rooster.bmp")
+        self.roosterImgs_jump = [self.__load_img(f"rsc/img/entities/rooster/rooster_jump{i + 1}.bmp") for i in range(2)]
 
-        self.coinImgs = [self.__load_img(f"rsc/img/objects/coin_animated{i+1}.bmp") for i in range(4)]
-        self.shitImgs = [self.__load_img(f"rsc/img/objects/shit_animated{i+1}.bmp") for i in range(4)]
-        self.eggImgs = [self.__load_img(f"rsc/img/objects/egg_animated{i+1}.bmp") for i in range(4)]
-        self.mushroomImgs = [self.__load_img(f"rsc/img/objects/mushroom_animated{i+1}.bmp") for i in range(2)]
+        self.coinImgs = [self.__load_img(f"rsc/img/objects/coin/coin_animated{i + 1}.bmp") for i in range(4)]
+        self.shitImgs = [self.__load_img(f"rsc/img/objects/shit/shit_animated{i + 1}.bmp") for i in range(4)]
+        self.eggImgs = [self.__load_img(f"rsc/img/objects/egg/egg_animated{i + 1}.bmp") for i in range(4)]
+        self.mushroomImgs = [self.__load_img(f"rsc/img/objects/mushroom/mushroom_animated{i + 1}.bmp") for i in
+                             range(2)]
 
         self.backgroundMusic = self.__load_audio('rsc/sounds/cyber-farm-271090.mp3', 0.5)
 
